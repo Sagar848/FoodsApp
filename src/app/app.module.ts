6ad7ff835a066a8fecaedeb2ce0a5256b93d206a
Sagar Poojary <sagarpoojary1@gmail.com>
2020-02-22 22:04:21 +0530

    added all components for recipe and shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './header/recipe/recipe.component';
+import { ShoppingComponent } from './header/shopping/shopping.component';
+import { ShoppingListComponent } from './header/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './header/recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
